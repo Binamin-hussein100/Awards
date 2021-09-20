28622ab3f83bb113636f0829f6372eae9d78e3e6
Binamin-hussein100 <binamin.hassan@student.moringaschool.com>
2021-09-20 17:31:29 +0300

    added a profile model

--- award/models.py
+++ award/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.db.models.signals import post_save
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,null=True,on_delete=models.CASCADE,default=None,blank=True)
+    bio = models.CharField(max_length=100)
+    profile_pic = CloudinaryField('image')
+    
+    def __str__(self):
+        return f'{self.user.username}'
+    
+    def save(self):
+        super().save()
+        
+    def save_profile(self):
+        self.save()
+        
+    def delete_profile(self):
+        self.delete()
+    
+    def profiles_posts(self):
+        self.image_set.all()
+        
+    def user_profile(sender,**kwargs):
+        if kwargs['created']:
+            prof = Profile.objects.create(user=kwargs['instance'])
+
+    post_save.connect(user_profile, sender=User)

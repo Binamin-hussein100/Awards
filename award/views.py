fef827233b95fffd0dd5ce5ec4fba2372d6239e4
Binamin-hussein100 <binamin.hassan@student.moringaschool.com>
2021-09-20 17:25:51 +0300

    added a register function

--- award/views.py
+++ award/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['password2']
+        
+        if password == confirm_password:
+            if User.objects.filter(username = username).exists():
+                messages.warning(request, 'Username already exists. Try another')
+                return redirect('register')
+            else:
+               if User.objects.filter(email = email).exists():
+                   messages.warning(request, 'Email already exists. Try another')
+                   return redirect('register')
+               else:
+                    user = User.objects.create_user(username=username,email=email,password=password)
+                    user.save()
+                    return redirect('login')

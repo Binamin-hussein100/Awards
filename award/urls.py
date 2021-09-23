57acdd9c07f28f55e75f4efaf33bc56e4faca03a
Binamin-hussein100 <binamin.hassan@student.moringaschool.com>
2021-09-23 18:34:19 +0300

    added a search url

--- award/urls.py
+++ award/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import register,login,index,prj_form, search
+from .views import register,login,index,prj_form, search,get_profile
 
 urlpatterns = [
     path('register/',register,name='register'),
     path('',login,name='login'),
     path('home/',index,name='home'),
     path('post/',prj_form,name='post'),
-    path('search/',search,name='search')
-    
+    path('search/',search,name='search'),
+    path('profile/',get_profile,name='profile')
 ]

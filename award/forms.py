c2561bd8d0b5b33883b4c4cb54ad7ba28e44c917
Binamin-hussein100 <binamin.hassan@student.moringaschool.com>
2021-09-23 18:36:09 +0300

    added a profile update form

--- award/forms.py
+++ award/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.forms import widgets
+from django.forms import fields, widgets
 from .models import Project,Profile
 
 # your code here
@@ -8,3 +8,11 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ['title','landing_page','description','link']
+
+
+
+class ProfileForm(forms.ModelForm):
+    
+    class Meta:
+        model = Profile
+        fields = ['bio','profile_pic']
